6fcd1b8d1f7c8a364115ca3cfe991554ec7803e6
rajatrar <43917713+rajatrar@users.noreply.github.com>
2025-07-08 15:14:51 +0000

    added js file changes

---
+++ guessTheNumber/script.js
@@ -0,0 +1,45 @@
+
+let number = Math.trunc(Math.random() * 20) + 1;
+let score = 20;
+let highscore = localStorage.getItem('highscore') ? Number(localStorage.getItem('highscore')) : 0;
+console.log(number);
+document.querySelector('.highscore').textContent = highscore;
+
+document.querySelector('.check').addEventListener('click',function() {
+    const guess = Number(document.querySelector('.guess').value)
+    if(!guess){
+        document.querySelector('.message').textContent = "No Number";
+    }
+    else if(guess === number){
+        document.querySelector('.message').textContent = "Correct Number!";
+        document.querySelector('body').style.backgroundColor = '#60b347';
+        document.querySelector('.number').textContent = number;
+        if (score > highscore) {
+            highscore = score;
+            localStorage.setItem('highscore', highscore);
+            document.querySelector('.highscore').textContent = highscore;
+        }
+    }
+    else if(guess > number){
+        document.querySelector('.message').textContent = "Too High!";
+        score--;
+        document.querySelector('.score').textContent = score;
+
+    }
+    else if(guess < number){
+        document.querySelector('.message').textContent = "Too Low!";
+        score--;
+        document.querySelector('.score').textContent = score;
+    }
+})
+
+document.querySelector('.again').addEventListener('click',function() {
+    score = 20;
+    number = Math.trunc(Math.random() * 20) + 1;
+    document.querySelector('.message').textContent = "Start guessing...";
+    document.querySelector('.score').textContent = score;
+    document.querySelector('.number').textContent = '?';
+    document.querySelector('.guess').value = '';
+    document.querySelector('body').style.backgroundColor = '#222';
+    document.querySelector('.highscore').textContent = highscore;
+});
